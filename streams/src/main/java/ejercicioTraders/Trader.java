c0bca770e27e874a408e0f73013358f455c496ac
usertar <ahmedmario@hotmail.es>
2024-10-02 21:07:48 +0200

    nueva tareas de traders

---
+++ streams/src/main/java/ejercicioTraders/Trader.java
@@ -0,0 +1,20 @@
+package ejercicioTraders;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode
+@Builder
+public class Trader {
+	private final String name;
+	private final String city;
+}
---
+++ streams/src/main/java/ejercicioTraders/Transaction.java
@@ -0,0 +1,27 @@
+package ejercicioTraders;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+
+@ToString
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode
+@Builder
+public class Transaction {
+	private final Trader trader;
+	private final int year;
+	private final int value;
+	
+	
+
+	public String toString(){
+		return "{" + this.trader + ", " +
+				"year: "+this.year+", " +
+				"value:" + this.value +"}";
+	}
+}
