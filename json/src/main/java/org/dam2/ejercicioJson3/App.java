523cf598f2a545c6048b3329fd1e66dff707c7ea
ahmedhassank <ahmedmario@hotmail.es>
2024-11-02 10:24:40 +0100

    comienzo del ejercicio 3 de jsons

---
+++ json/src/main/java/org/dam2/ejercicioJson3/App.java
@@ -0,0 +1,9 @@
+package org.dam2.ejercicioJson3;
+
+public class App {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ json/src/main/java/org/dam2/ejercicioJson3/Localidades.java
@@ -0,0 +1,5 @@
+package org.dam2.ejercicioJson3;
+
+public class Localidades {
+
+}
