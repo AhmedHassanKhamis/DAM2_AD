b6df1afaa7c89650cbf464b26ec5c166e361c21f
usertar <ahmedmario@hotmail.es>
2025-01-29 21:05:19 +0100

    nuevas pruebas con un nuevo project emple depart

--- pruebaspringBanco/src/main/java/org/dam2/pruebaspringBanco/repositorio/ClienteRepositorio.java
+++ pruebaspringBanco/src/main/java/org/dam2/pruebaspringBanco/repositorio/ClienteRepositorio.java
@@ -9,6 +9,7 @@ import org.dam2.pruebaspringBanco.modelo.Telefono;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClienteRepositorio extends CrudRepository<Cliente, String> {
@@ -18,7 +19,7 @@ public interface ClienteRepositorio extends CrudRepository<Cliente, String> {
 	@Query("SELECT c.telefonos FROM Cliente c where c.nif = ?1")
 	List<Telefono> listadoTelefonos(String nia);
 	
-	
+	@Transactional(readOnly = true)
 	@Query("Select t.compania as compania, COUNT(t.compania) as total from Telefono t group by t.compania order by total desc")
 	Stream<CompaniaTotal> mayorCompania();
 }
